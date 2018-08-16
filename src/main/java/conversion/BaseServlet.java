8e1385e92b35d1811ad677eec9258136bc05d7e8
Patrick <PatrickSheppard@idrsolutions.com>
2018-08-16 15:46:52 +0100

    Adding javadoc to base server.

--- src/main/java/conversion/BaseServlet.java
+++ src/main/java/conversion/BaseServlet.java
@@ -31,6 +31,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+/**
+ * An extendable base for conversion microservices. Provides general
+ * functionality for polling, file upload/download, initial creation of files
+ * and UUID's.
+ */
 public abstract class BaseServlet extends HttpServlet {
 
     private static final Logger LOG = Logger.getLogger(BaseServlet.class.getName());
@@ -42,6 +47,18 @@ public abstract class BaseServlet extends HttpServlet {
 
     private final ExecutorService queue = Executors.newFixedThreadPool(5);
 
+    /**
+     * Sets the status of the response to the given error status. This method
+     * should be called when setting error statuses on responses is called for.
+     * No Checking is done to make sure the error status is correct or in the
+     * right range.
+     *
+     * @param response
+     * @param error
+     * @param status
+     * @throws IOException if the error message cannot be written to the
+     * response.
+     */
     private static void doError(final HttpServletResponse response, final String error, final int status) throws IOException {
         response.setContentType("application/json");
         response.setStatus(status);
@@ -50,6 +67,21 @@ public abstract class BaseServlet extends HttpServlet {
         }
     }
 
+    /**
+     * Get request to the servlet. Used here for the purpose of polling the
+     * servlet for updates on progress. A UUID (unique user ID) must be provided
+     * by the client. This UUID is received when beginning a conversion request.
+     * <p>
+     * If no UUID or an unknown UUID is provided then a 404 response is
+     * generated. The response contains a json string defined by
+     * {@link Individual#toJsonString()}.
+     *
+     * @param request
+     * @param response
+     * @throws IOException if the error message cannot be written to the
+     * response.
+     * @see Individual#toJsonString()
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -74,17 +106,46 @@ public abstract class BaseServlet extends HttpServlet {
         }
     }
 
+    /**
+     * Responds with the communication methods that this server supports.
+     *
+     * @param request
+     * @param response
+     * @see BaseServlet#allowCrossOrigin(HttpServletResponse)
+     */
     @Override
     protected void doOptions(HttpServletRequest request, HttpServletResponse response) {
         allowCrossOrigin(response);
     }
 
+    /**
+     * Allow cross origin requests according to the CORS standard.
+     *
+     * @param response
+     */
     private void allowCrossOrigin(final HttpServletResponse response) {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE");
         response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin");
     }
 
+    /**
+     * A post request to the server. This method deals with the initial contact
+     * with the client. It looks for a file within the http request.
+     * <p>
+     * If a file is not present a 400 error is returned. If the file cannot be
+     * parsed from the http request then a 500 is returned.
+     * <p>
+     * If the file is present then an input dir and and output dir for that file
+     * are created (overwritten if they already exist).
+     * <p>
+     * The convert() method is then started in a separate thread.
+     *
+     * @param request
+     * @param response
+     * @see BaseServlet#convert(Individual, Map, String, String, String, String,
+     * String, String)
+     */
     @Override
     protected void doPost(final HttpServletRequest request, final HttpServletResponse response) {
 
@@ -176,10 +237,37 @@ public abstract class BaseServlet extends HttpServlet {
         }
     }
 
+    /**
+     * This method converts a file and writes it to the output directory under
+     * the Individuals UUID. It is called at the point when a valid user file is
+     * stored in the input directory.
+     * <p>
+     * No Validation is done on the file when this method is called and it is up
+     * to the implementing class to determine if the given file is actually a
+     * pdf. Is it also up to the implementing class to generate preview/download
+     * urls, zip the output, and set the individual state to processed.
+     *
+     * @param individual Internal representation of individual who made this
+     * request.
+     * @param parameterMap the map of parameters from the request.
+     * @param fileName
+     * @param inputDirectory
+     * @param outputDirectory
+     * @param fileNameWithoutExt
+     * @param ext
+     * @param contextURL The url from the protocol up to the servlet url url
+     * pattern.
+     */
     abstract void convert(final Individual individual, final Map<String, String[]> parameterMap, final String fileName,
             final String inputDirectory, final String outputDirectory,
             final String fileNameWithoutExt, final String ext, final String contextURL);
 
+    /**
+     * Get the filename of the file contained in this request part.
+     *
+     * @param part
+     * @return the file name or null if it does not exist.
+     */
     private String getFileName(final Part part) {
         for (String content : part.getHeader("content-disposition").split(";")) {
             if (content.trim().startsWith("filename")) {
@@ -202,6 +290,14 @@ public abstract class BaseServlet extends HttpServlet {
         return full.substring(0, full.length() - request.getServletPath().length());
     }
 
+    /**
+     * Get the conversion parameters. The parameter key value pairs are held in
+     * a semi-colon separated list ";" with a colon separating the individual
+     * key value pairs. E.g. "key1:val1;key2:val2;etc..."
+     *
+     * @param settings
+     * @return a String array in the form [key1, val1, key2, val2, etc...]
+     */
     protected static String[] getConversionParams(final String settings) {
         if (settings == null) {
             return null;
@@ -217,6 +313,11 @@ public abstract class BaseServlet extends HttpServlet {
         return result;
     }
 
+    /**
+     * Delete a folder and all of its contents.
+     *
+     * @param dirPath
+     */
     private static void deleteFolder(final File dirPath) {
         final File[] files = dirPath.listFiles();
         if (files != null) {
@@ -229,6 +330,12 @@ public abstract class BaseServlet extends HttpServlet {
         }
     }
 
+    /**
+     * Update the progress of the conversion. This method is usually called
+     * after a poll from a client to the servlet.
+     *
+     * @param individual
+     */
     abstract void updateProgress(final Individual individual);
 
 }
