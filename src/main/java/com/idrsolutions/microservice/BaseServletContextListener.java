d58c75f2a72cd4f8019d38b7b5809d512d0fea35
Leon Atherton <leon@idrsolutions.com>
2019-10-16 14:55:46 +0100

    Fix memory leak caused by failing to stop the thread-pools when undeployed

---
+++ src/main/java/com/idrsolutions/microservice/BaseServletContextListener.java
@@ -0,0 +1,37 @@
+package com.idrsolutions.microservice;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+@WebListener
+public class BaseServletContextListener implements ServletContextListener {
+
+    private static final int PCOUNT = Runtime.getRuntime().availableProcessors();
+
+    private final ExecutorService convertQueue = Executors.newFixedThreadPool(PCOUNT);
+    private final ExecutorService downloadQueue = Executors.newFixedThreadPool(5);
+    private final ScheduledExecutorService callbackQueue = Executors.newScheduledThreadPool(5);
+
+    @Override
+    public void contextInitialized(final ServletContextEvent servletContextEvent) {
+        final ServletContext servletContext = servletContextEvent.getServletContext();
+
+        servletContext.setAttribute("convertQueue", convertQueue);
+        servletContext.setAttribute("downloadQueue", downloadQueue);
+        servletContext.setAttribute("callbackQueue", callbackQueue);
+    }
+
+    @Override
+    public void contextDestroyed(final ServletContextEvent servletContextEvent) {
+        final ServletContext servletContext = servletContextEvent.getServletContext();
+
+        ((ExecutorService) servletContext.getAttribute("convertQueue")).shutdownNow();
+        ((ExecutorService) servletContext.getAttribute("downloadQueue")).shutdownNow();
+        ((ExecutorService) servletContext.getAttribute("callbackQueue")).shutdownNow();
+    }
+}
