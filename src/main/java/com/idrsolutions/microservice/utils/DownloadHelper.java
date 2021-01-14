2bb2a1263cec179f8bfc733adb1bd45e432c885e
AmySuzannePearson <amy@idrsolutions.com>
2021-01-14 12:21:08 +0000

    update the copyright date to 2021

--- src/main/java/com/idrsolutions/microservice/utils/DownloadHelper.java
+++ src/main/java/com/idrsolutions/microservice/utils/DownloadHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Patrick Sheppard.
+ * Copyright 2021 IDRSolutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
