9b1d9d6628efdd9b3714aae03f805813adf4242a
Kieran-IDR <kieran@idrsolutions.com>
2021-11-23 10:34:11 +0000

    Update the prototype to correctly load individual settings files

--- src/main/java/com/idrsolutions/microservice/BaseServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/BaseServletContextListener.java
@@ -1,25 +1,30 @@
 package com.idrsolutions.microservice;
 
+import com.idrsolutions.microservice.utils.PropertiesHelper;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-@WebListener
 public class BaseServletContextListener implements ServletContextListener {
 
-    private static final int PCOUNT = Runtime.getRuntime().availableProcessors();
-
-    private final ExecutorService convertQueue = Executors.newFixedThreadPool(PCOUNT);
-    private final ExecutorService downloadQueue = Executors.newFixedThreadPool(5);
-    private final ScheduledExecutorService callbackQueue = Executors.newScheduledThreadPool(5);
+    String propertiesFile;
 
     @Override
     public void contextInitialized(final ServletContextEvent servletContextEvent) {
+
         final ServletContext servletContext = servletContextEvent.getServletContext();
+
+        PropertiesHelper.loadProperties(servletContext, propertiesFile);
+
+        BaseServlet.setInputPath((String) servletContext.getAttribute("service.inputLocation"));
+
+        final ExecutorService convertQueue = Executors.newFixedThreadPool((Integer) servletContext.getAttribute("service.concurrentConversion"));
+        final ExecutorService downloadQueue = Executors.newFixedThreadPool(5);
+        final ScheduledExecutorService callbackQueue = Executors.newScheduledThreadPool(5);
 
         servletContext.setAttribute("convertQueue", convertQueue);
         servletContext.setAttribute("downloadQueue", downloadQueue);
---
+++ src/main/java/com/idrsolutions/microservice/utils/PropertiesHelper.java
@@ -0,0 +1,70 @@
+package com.idrsolutions.microservice.utils;
+
+import com.idrsolutions.microservice.BaseServlet;
+
+import javax.servlet.ServletContext;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesHelper {
+
+    public static void loadProperties(final ServletContext servletContext, final String propertiesFile) {
+
+        final Properties properties = readPropertiesFile(propertiesFile);
+
+        //service.concurrentConversion
+        loadConcurrentConversions(servletContext, properties);
+
+
+        //service.inputLocation
+        loadInputLocation(servletContext, properties);
+
+        //service.outputLocation
+
+    }
+
+    private static Properties readPropertiesFile(final String propertiesFile) {
+        final Properties properties = new Properties();
+
+        try (FileInputStream propertiesInput = new FileInputStream(propertiesFile)){
+            properties.load(propertiesInput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    private static void loadConcurrentConversions(final ServletContext servletContext, final Properties properties) {
+        String concurrentConversions = properties.getProperty("service.concurrentConversion");
+        final int concurrentConversionCount;
+        if (concurrentConversions != null && !concurrentConversions.isEmpty() && concurrentConversions.matches("\\d+")) {
+            concurrentConversionCount = Integer.parseInt(concurrentConversions);
+        } else {
+            concurrentConversionCount = Runtime.getRuntime().availableProcessors();
+        }
+
+        servletContext.setAttribute("service.concurrentConversion", concurrentConversionCount);
+    }
+
+    private static void loadInputLocation(final ServletContext servletContext, final Properties properties) {
+
+
+        String propertiesInputLocation = properties.getProperty("service.inputLocation");
+        final String inputLocation;
+        if (propertiesInputLocation != null && !propertiesInputLocation.isEmpty()) {
+            inputLocation = propertiesInputLocation;
+        } else {
+            String userDir = System.getProperty("user.home");
+            if (!userDir.endsWith("/") && !userDir.endsWith("\\")) {
+                userDir += System.getProperty("file.separator");
+            }
+
+            inputLocation = userDir + "/.idr/buildvu-microservice/input/";
+        }
+
+        servletContext.setAttribute("service.inputLocation", inputLocation);
+    }
+
+}
