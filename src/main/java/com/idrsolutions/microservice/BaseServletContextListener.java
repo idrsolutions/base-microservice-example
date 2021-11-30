7cf9f51d70f6ceaa9bdcaebf070d8566ab2001cb
Kieran-IDR <kieran@idrsolutions.com>
2021-11-30 09:23:59 +0000

    Update config file to decentralise properties validation

--- src/main/java/com/idrsolutions/microservice/BaseServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/BaseServletContextListener.java
@@ -1,7 +1,5 @@
 package com.idrsolutions.microservice;
 
-import com.idrsolutions.microservice.utils.PropertiesHelper;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -9,6 +7,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -26,24 +25,37 @@ public abstract class BaseServletContextListener implements ServletContextListen
     @Override
     public void contextInitialized(final ServletContextEvent servletContextEvent) {
 
+        final Properties propertiesFile = new Properties();
         final ServletContext servletContext = servletContextEvent.getServletContext();
         final File externalFile = new File(getConfigPath() + getConfigName());
-        try {
-            final InputStream propertiesFile;
-            if (externalFile.exists()) {
-                propertiesFile = new FileInputStream(externalFile.getAbsolutePath());
-            } else {
-                propertiesFile = servletContextEvent.getServletContext().getResourceAsStream("buildvu-microservice.properties");
-            }
 
-            PropertiesHelper.loadProperties(servletContext, propertiesFile);
-            propertiesFile.close();
-
+        try(final InputStream intPropertiesFile = servletContext.getResourceAsStream(getConfigName())) {
+            propertiesFile.load(intPropertiesFile);
         } catch (IOException e) {
-            LOG.log(Level.SEVERE, "IOException thrown when reading properties file", e);
+            LOG.log(Level.SEVERE, "IOException thrown when reading default properties file", e);
         }
 
-        final ExecutorService convertQueue = Executors.newFixedThreadPool((Integer) servletContext.getAttribute("service.concurrentConversion"));
+        if (externalFile.exists()) {
+            try (final InputStream extPropertiesFile = new FileInputStream(externalFile.getAbsolutePath())) {
+                propertiesFile.load(extPropertiesFile);
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, "IOException thrown when reading external properties file", e);
+            }
+        }
+
+        servletContext.setAttribute("properties", propertiesFile);
+
+        final String concurrentConversions = propertiesFile.getProperty("service.concurrentConversion");
+        final int concurrentConversionCount;
+        if (concurrentConversions != null && !concurrentConversions.isEmpty() && concurrentConversions.matches("\\d+") && Integer.parseInt(concurrentConversions) > 0) {
+            concurrentConversionCount = Integer.parseInt(concurrentConversions);
+        } else {
+            concurrentConversionCount = Runtime.getRuntime().availableProcessors();
+            final String logDefaultUse = "Properties value for \"service.concurrentConversion\" incorrect, should be a positive integer. Using a value of " + concurrentConversionCount + " based on available processors";
+            LOG.log(Level.SEVERE, logDefaultUse);
+        }
+
+        final ExecutorService convertQueue = Executors.newFixedThreadPool(concurrentConversionCount);
         final ExecutorService downloadQueue = Executors.newFixedThreadPool(5);
         final ScheduledExecutorService callbackQueue = Executors.newScheduledThreadPool(5);
 
