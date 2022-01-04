1978976652ab78173b9f73123a92bda67a4987a6
Jacob Lucas <Jacob@IDRSolutions.com>
2022-01-04 12:25:44 +0000

    Final Finality

--- src/main/java/com/idrsolutions/microservice/utils/ZipHelper.java
+++ src/main/java/com/idrsolutions/microservice/utils/ZipHelper.java
@@ -73,7 +73,7 @@ public class ZipHelper {
      * @throws IOException if there is a problem writing or reading the file
      */
     public static byte[] zipFolderInMemory(final String srcFolder, final boolean createParentDirectoryInZip) throws IOException {
-        try (final ByteArrayOutputStream zipBAOS = new ByteArrayOutputStream()) {
+        try (ByteArrayOutputStream zipBAOS = new ByteArrayOutputStream()) {
             try (ZipOutputStream zip = new ZipOutputStream(zipBAOS)) {
                 addFolderToZip(createParentDirectoryInZip ? new File(srcFolder).getName() + '/' : "", srcFolder, zip);
                 zip.flush();
