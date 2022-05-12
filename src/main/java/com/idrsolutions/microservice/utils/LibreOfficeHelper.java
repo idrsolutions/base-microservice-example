8913a6bd5b05aa4763cf718fc16b4aaa158a8f5c
Kieran <kieran@idrsolutions.com>
2022-05-12 15:08:04 +0100

    Add new property to set timeout for LibreOffice

--- src/main/java/com/idrsolutions/microservice/utils/LibreOfficeHelper.java
+++ src/main/java/com/idrsolutions/microservice/utils/LibreOfficeHelper.java
@@ -51,6 +51,20 @@ public class LibreOfficeHelper {
      * occurs
      */
     public static boolean convertToPDF(final String sofficePath, final File file, final String uuid) {
+        return convertToPDF(sofficePath, file, uuid, 60000);
+    }
+
+    /**
+     * Converts an office file to PDF using the specified LibreOffice executable.
+     *
+     * @param sofficePath The path to the soffice executable
+     * @param file The office file to convert to PDF
+     * @param uuid The uuid of the conversion on which to set the error if one occurs
+     * @param timeoutDuration The timeout duration for libreoffice conversions in milliseconds
+     * @return true on success, false on failure
+     * occurs
+     */
+    public static boolean convertToPDF(final String sofficePath, final File file, final String uuid, final long timeoutDuration) {
         final String uniqueLOProfile = TEMP_DIR.replace('\\', '/') + "LO-" + uuid;
 
         final ProcessBuilder pb = new ProcessBuilder(sofficePath,
@@ -61,7 +75,7 @@ public class LibreOfficeHelper {
 
         try {
             final Process process = pb.start();
-            if (!process.waitFor(1, TimeUnit.MINUTES)) {
+            if (!process.waitFor(timeoutDuration, TimeUnit.MILLISECONDS)) {
                 process.destroy();
                 DBHandler.getInstance().setError(uuid, 1050, "Libreoffice timed out after 1 minute");
                 return false;
